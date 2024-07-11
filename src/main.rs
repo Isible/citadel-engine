8623badac1f0250bd4562a2e74d03d641a759dd0
thepigcat <benpospo@gmail.com>
2024-07-11 22:36:06 +0200

    finish move

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
-use std::{env, fs, io, path::PathBuf, str};
+use std::{env, fs, path::PathBuf};
 
 use bumpalo::Bump;
-use citadel_api::{backend::asm::{AsmBackend, TargetX86_64}, compile};
+use citadel_api::{
+    backend::asm::{AsmBackend, TargetX86_64},
+    compile,
+};
 use citadel_irparser::{IRLexer, IRParser};
 
-fn main() -> io::Result<()> {
+fn main() {
     run()
 }
 
-fn run() -> io::Result<()> {
-    let mut path = path_from_arg().expect("User needs to specify a path to the file containing the IR");
-    let file_content = fs::read(&path)?;
-    let lexer = IRLexer::new(str::from_utf8(&file_content).unwrap());
+fn run() {
+    let file_content =
+        fs::read(path_from_arg()).expect("User needs to specify a path to the file containing the IR");
+    let lexer = IRLexer::new(std::str::from_utf8(&file_content).unwrap());
     let arena = Bump::new();
     let mut parser = IRParser::new(&lexer, &arena);
     let ir_stream = parser.parse_program();
-    dbg!(&ir_stream);
-    path.set_extension(".asm");
-    compile!(AsmBackend::new(TargetX86_64), ir_stream);
-    Ok(())
+    compile!(AsmBackend::new(TargetX86_64), ir_stream)
+        .to_file(PathBuf::from("out.asm"))
+        .expect("Failed to create compiled asm file");
 }
 
-fn path_from_arg() -> Option<PathBuf> {
-    let path = env::args().into_iter().nth(1)?;
-    Some(path.into())
+fn path_from_arg() -> PathBuf {
+    env::args()
+        .into_iter()
+        .nth(1)
+        .map(|arg| arg.into())
+        .unwrap_or(PathBuf::from("main.chir"))
 }
